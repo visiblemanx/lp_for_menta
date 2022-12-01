b28103d534431ff6eb6c9161d859cbb290c60548
s.negishi <s.negishi@diverstage.biz>
2022-12-01 11:35:44 +0900

    modify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: 3201, host: true },
-  integrations: [react()],
 });
