b8087a56cab2015acdae2d388f9420939fa52bcd
s.negishi <s.negishi@diverstage.biz>
2022-12-01 10:23:44 +0900

    add footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,6 @@
 import { defineConfig } from 'astro/config';
-import { inject } from '@vercel/analytics';
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3201, host: true },
-  integrations: [
-    inject(),
-  ]
+  server: { port: 3201, host: true }
 });
