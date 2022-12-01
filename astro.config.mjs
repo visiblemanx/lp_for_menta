dbb85ac3e4ced30e36fe3a0b4475d16f0b74f789
s.negishi <s.negishi@diverstage.biz>
2022-12-01 10:10:33 +0900

    modify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from 'astro/config';
+import { inject } from '@vercel/analytics';
 
 // https://astro.build/config
 export default defineConfig({
-  server: { port: 3201, host: true }
+  server: { port: 3201, host: true },
+  integrations: [
+    inject(),
+  ]
 });
